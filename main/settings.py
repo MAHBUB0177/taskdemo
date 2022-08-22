f09dfd1dd6d52c2a98c02d5543f471c9b4fc5333
Mahbub Alam <ma01776879@gmail.com>
2022-08-22 11:39:56 +0600

    ok

--- main/settings.py
+++ main/settings.py
@@ -83,24 +83,24 @@ REST_FRAMEWORK = {
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'task1',
-#         'USER': 'task1',
-#         'PASSWORD': 'task1',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#         'ATOMIC_REQUESTS': True,
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'task1',
+        'USER': 'task1',
+        'PASSWORD': 'task1',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        'ATOMIC_REQUESTS': True,
+    }
+}
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
